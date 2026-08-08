37d8967e566469ea6548949ab8cf3ec041c1a4f3
logeshOffical <iamlogeshwaran.info@gmail.com>
2026-08-08 11:35:03 +0530

    Fix: add BrowserRouter basename for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Contact from "./pages/Contact";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/CanaEventPlanners">
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
